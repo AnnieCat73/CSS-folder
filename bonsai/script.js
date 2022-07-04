a05e4a2fad648c6f60f5d72e5725f7e42f2f0b5b
Annie  Indreiten <devannie777@gmail.com>
2022-07-04 06:42:00 +0100

    Toggle initial functionality for button

--- bonsai/script.js
+++ bonsai/script.js
@@ -14,3 +14,26 @@ openBtn.addEventListener('click', () => {
 })
 
 
+//Toggle
+
+const toggleBtn = document.querySelector(".toggle-ball");
+const price1 = document.querySelector("#price-1");
+const price2 = document.querySelector("#price-2");
+const price3 = document.querySelector("#price-3");
+
+
+toggleBtn.addEventListener("click", e => {
+  toggleBtn.classList.toggle("toggle-active");
+  /**if(toggleBtn) {
+   * price1.innerText = '17';
+   * price2.innerText = '32'
+   * price.3.innerText = '52'} 
+   * else {
+   * price1.innerText = '24';
+   * price2.innerText = '39'
+   * price.3.innerText = '79'
+   * } */
+
+})
+
+
